01be987507bd45209f16bba094d71f3525a989b5
Max Slipchuk <maxslipchuk11@gmail.com>
2026-02-21 13:45:58 +0200

    Fix copy

--- ConsoleS3/ConsoleS3/Program.cs
+++ ConsoleS3/ConsoleS3/Program.cs
@@ -7,6 +7,26 @@ Console.InputEncoding = System.Text.Encoding.UTF8;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.WriteLine("--Робота з S3--");
+var credentials = new BasicAWSCredentials(
+    "AKIAY7XUAOGIWNUDR573",
+    "2sVViQ/0snugHLDSECIlsLx+D+Yh6ynHy7Pg9Uk1");
+
+var client = new AmazonS3Client(credentials, RegionEndpoint.EUCentral1);
+
+string dir = "images";
+string path = Path.Combine(dir, "image.jpg");
+
+string bucketName = "my-p32bucket";
+
+var request = new PutObjectRequest
+{
+    BucketName = bucketName,
+    Key = "images/image.jpg",
+    FilePath = path,
+    ContentType = "image/jpeg"
+};
+
+await client.PutObjectAsync(request);
 
 
 
