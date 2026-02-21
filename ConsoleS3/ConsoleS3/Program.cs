85751dbd9634d2ba1775221beca7e5bc63674681
Max Slipchuk <maxslipchuk11@gmail.com>
2026-02-21 13:07:03 +0200

    Working console App

---
+++ ConsoleS3/ConsoleS3/Program.cs
@@ -0,0 +1,12 @@
+﻿using Amazon;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("--Робота з S3--");
+
+
+
